1fae2c56a1aa2e990b2794b7034457eeb3d5af01
UmmerZ <69335478+UmmerZ@users.noreply.github.com>
2021-03-08 21:30:40 -0700

    added basename to router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css"
 import React from "react"
-import HomePage from "./components/FrontPage"
+import HomePage from "./components/HomePage"
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import AboutMe from "./components/AboutMe"
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about-me" component={AboutMe} />
