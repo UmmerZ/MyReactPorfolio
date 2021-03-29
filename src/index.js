05d26966eec2296c511c34d43fdcd9f063cfd5fe
UmmerZ <69335478+UmmerZ@users.noreply.github.com>
2021-03-29 02:33:17 -0600

    Fixing error

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 )
 
